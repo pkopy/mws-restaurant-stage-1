51be2acf6b24aee434613234e2258506ccb569a4
kopycki <pawel.kopycki@gmail.com>
2018-07-20 15:24:17 +0200

    test service work

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+    self.addEventListener('fetch', (event) => {
+        console.log(event.request)
+    })
+       
+    //     event.respondWith(
+    //         fetch(event.request).then(function(response) {
+    //             if (response.status === 404) {
+    //               // TODO: instead, respond with the gif at
+    //               // /imgs/dr-evil.gif
+    //               // using a network request
+    //               return new Response("Whoops, not found");
+    //             }
+    //             return response;
+    //           }).catch(function() {
+    //             return new Response("Uh oh, that totally failed!");
+    //           })
+            
+    //     );
+    // })
--- js/main.js
+++ js/main.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
 serWork = () => {
   if (!navigator.serviceWorker) return;
   
-  navigator.serviceWorker.register('js/index.js').then(function(reg) {
+  navigator.serviceWorker.register('/index.js').then(function(reg) {
     console.log('Registration worked!');
   }).catch(function(reg) {
-    console.log('Registration failed!', reg);
+    console.log('Registration failed!');
   });
   
 }
